2bf2628812dcce7d455a3ce238b06193817369c9
tehkohrz <dongkunkoh@hotmail.com>
2022-09-27 23:11:24 +0800

    Chore: Updates to include ChakraUI as a package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './Routes';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <React.Suspense fallback={null}>
         <Router basename='/'>
           <Routes>
@@ -18,7 +19,7 @@ function App() {
           </Routes>
         </Router>
       </React.Suspense>
-    </>
+    </ChakraProvider>
   );
 }
 
