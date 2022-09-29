92900f8ae6dff7cc9d61905b731547caf321b79a
tehkohrz <dongkunkoh@hotmail.com>
2022-09-29 11:37:02 +0800

    Feat: Added chakra profile fields

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { useLocation, Navigate } from "react-router-dom";
-// import { Box, Button } from "@mui/material";
-import { Navbar } from "./components/organisms/Navbar";
-import Signup from "./components/pages/user/Signup";
-import Map from "./components/pages/walk/Map";
-import Profile from "./components/pages/user/Profile";
-import Signin from "./components/pages/user/Signin";
-import EditProfile from "./components/pages/user/EditProfile";
-import Friends from "./components/pages/friends/Friends";
+import React from 'react';
+import { useLocation, Navigate } from 'react-router-dom';
+import { Navbar } from './components/organisms/Navbar';
+import Signup from './components/pages/user/Signup';
+import Map from './components/pages/walk/Map';
+import Profile from './components/pages/user/Profile';
+import Signin from './components/pages/user/Signin';
+import EditProfile from './components/pages/user/EditProfile';
+import Friends from './components/pages/friends/Friends';
 
-import { useAuth } from "./hooks/use-auth";
+import { useAuth } from './hooks/use-auth';
 
 function RequireAuth({ children }) {
   const { isAuthenticated } = useAuth();
   const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate to="/signin" state={{ from: location }} replace />;
+    return <Navigate to='/signin' state={{ from: location }} replace />;
   }
 
   return children;
@@ -24,59 +23,49 @@ function RequireAuth({ children }) {
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     element: <Navbar />,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: (
       <>
-        <Navbar />
         <Signup />
       </>
     ),
   },
   {
-    path: "/signin",
+    path: '/signin',
     element: (
       <>
-        <Navbar />
         <Signin />
       </>
     ),
   },
   {
-    path: "/profile",
-    element: (
-      <>
-        <Navbar />
-        <RequireAuth>{<Profile />}</RequireAuth>
-      </>
-    ),
+    path: '/profile',
+    element: <>{<Profile />}</>,
   },
   {
-    path: "/editProfile",
+    path: '/editProfile',
     element: (
       <>
-        <Navbar />
         <RequireAuth>{<EditProfile />}</RequireAuth>
       </>
     ),
   },
   {
-    path: "/friends",
+    path: '/friends',
     element: (
       <>
-        <Navbar />
         <RequireAuth>{<Friends />}</RequireAuth>
       </>
     ),
   },
   {
-    path: "/walk/map",
+    path: '/walk/map',
     element: (
       <>
-        <Navbar />
         <Map />
       </>
     ),
