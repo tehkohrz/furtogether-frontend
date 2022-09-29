4509ca46d5d8ab75ae3bbb6d490da7e3938ddbc6
tehkohrz <dongkunkoh@hotmail.com>
2022-09-29 11:50:02 +0800

    Chore: Cleaned up duplicated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 // import routes from './Routes';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useAuth } from './hooks/use-auth';
-import { Navbar } from './components/organisms'
+import { Navbar } from './components/organisms';
 import { Registration, Home } from './components/pages';
+import { Profile } from './components/pages/user/Profile';
 
 const routes = [
   // {
@@ -16,9 +23,18 @@ const routes = [
     path: '/',
     element: <Registration />,
   },
-]
+];
 
 const authenticatedRoutes = [
+  {
+    path: '/profile',
+    element: (
+      <>
+        <Navbar />
+        <RequireAuth>{<Profile />}</RequireAuth>
+      </>
+    ),
+  },
   // {
   //   path: '/addstudent',
   //   element: <AddStudent />,
@@ -27,16 +43,16 @@ const authenticatedRoutes = [
   //   path: '/attendance',
   //   element: <Attendance />,
   // },
-]
+];
 
-function RequireAuth({  children }) {
+function RequireAuth({ children }) {
   const { isAuthenticated } = useAuth();
   const location = useLocation();
-  
-  console.log('isAuthenticated: ', isAuthenticated)
+
+  console.log('isAuthenticated: ', isAuthenticated);
   if (!isAuthenticated) {
     // write path of sign-in page here instead of '/'
-    return <Navigate to="/" state={{ from: location }} replace />;
+    return <Navigate to='/' state={{ from: location }} replace />;
   }
 
   return children;
@@ -49,16 +65,20 @@ function App() {
         <Navbar />
         <Router basename='/'>
           <Routes>
-          {routes.map((route) => (
-            <Route key={route} path={route.path} element={route.element}></Route>
-          ))}
-          {authenticatedRoutes.map((route) => (
-            <Route
-              key = {route}
-              path={route.path}
-              exact
-              element={<RequireAuth >{route.element}</RequireAuth>}
-            />
+            {routes.map((route) => (
+              <Route
+                key={route}
+                path={route.path}
+                element={route.element}
+              ></Route>
+            ))}
+            {authenticatedRoutes.map((route) => (
+              <Route
+                key={route}
+                path={route.path}
+                exact
+                element={<RequireAuth>{route.element}</RequireAuth>}
+              />
             ))}
           </Routes>
         </Router>
