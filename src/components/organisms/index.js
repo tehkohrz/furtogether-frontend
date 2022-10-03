6b1466ded5e294f99b88163475558e981d981103
richiechia <richiechia@gmail.com>
2022-10-04 03:06:30 +0800

    fix: ProfileTab update

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,5 +1,4 @@
 export { default as SignUp } from './SignUp'
 export { default as Login } from './Login'
 export { default as Navbar } from './Navbar'
-export { default as PhotoAlbum } from './PhotoAlbum'
 export { default as TabsComponent } from './TabsComponent'
